795cd3b130eb0a650d3177d34e5a858fdbf2bdea
kali-tan <tank4@vcu.edu>
2023-11-25 19:38:01 -0500

    added my final

---
+++ final/script.js
@@ -0,0 +1,47 @@
+$(".enraged").hover(
+    function(){
+        $(".enraged").toggleClass("enragedaction")
+    }
+);
+
+$(".anxious").hover(
+    function(){
+        $(".anxious").toggleClass("anxiousaction")
+    }
+)
+
+$(".alive").hover(
+    function(){
+        $(".alive").toggleClass("aliveaction")
+    }
+)
+
+$(".hopeful").hover(
+    function(){
+        $(".hopeful").toggleClass("hopefulaction")
+    }
+)
+
+$(".grieving").hover(
+    function(){
+        $(".grieving").toggleClass("grievingaction")
+    }
+)
+
+$(".lonely").hover(
+    function(){
+        $(".lonely").toggleClass("lonelyaction")
+    }
+)
+
+$(".comfortable").hover(
+    function(){
+        $(".comfortable").toggleClass("comfortableaction")
+    }
+)
+
+$(".empathy").hover(
+    function(){
+        $(".empathy").toggleClass("empathyaction")
+    }
+)
