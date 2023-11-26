f5a6f82827fa262bd78e33d34349ef57840fa452
kali-tan <tank4@vcu.edu>
2023-11-25 20:51:19 -0500

    edits to final

--- final/script.js
+++ final/script.js
@@ -1,47 +1,70 @@
 $(".enraged").hover(
     function(){
-        $(".enraged").toggleClass("enragedaction")
+        $(".enraged").toggleClass("enragedaction");
     }
 );
 
 $(".anxious").hover(
     function(){
-        $(".anxious").toggleClass("anxiousaction")
+        $(".anxious").toggleClass("anxiousaction");
     }
 )
 
 $(".alive").hover(
     function(){
-        $(".alive").toggleClass("aliveaction")
+        $(".alive").toggleClass("aliveaction");
     }
 )
 
 $(".hopeful").hover(
     function(){
-        $(".hopeful").toggleClass("hopefulaction")
+        $(".hopeful").toggleClass("hopefulaction");
     }
 )
 
 $(".grieving").hover(
     function(){
-        $(".grieving").toggleClass("grievingaction")
+        $(".grieving").toggleClass("grievingaction");
     }
 )
 
 $(".lonely").hover(
     function(){
-        $(".lonely").toggleClass("lonelyaction")
+        $(".lonely").toggleClass("lonelyaction");
     }
 )
 
 $(".comfortable").hover(
     function(){
-        $(".comfortable").toggleClass("comfortableaction")
+        $(".comfortable").toggleClass("comfortableaction");
     }
 )
 
 $(".empathy").hover(
     function(){
-        $(".empathy").toggleClass("empathyaction")
+        $(".empathy").toggleClass("empathyaction");
+    }
+)
+
+$(".main-title").hover(
+    function(){
+        $(".main-title").toggleClass("pause");
+        $(".main-title").toggleClass("reverse-title");
+        $("body").toggleClass("reverse-body");
+        $(".enraged").toggleClass("invisible");
+        $(".anxious").toggleClass("invisible");
+        $(".alive").toggleClass("invisible");
+        $(".hopeful").toggleClass("invisible");
+        $(".grieving").toggleClass("invisible");
+        $(".lonely").toggleClass("invisible");
+        $(".comfortable").toggleClass("invisible");
+        $(".empathy").toggleClass("invisible");
+
+    }
+)
+
+$(".home").hover(
+    function(){
+        $(".home").toggleClass("active-home");
     }
 )
